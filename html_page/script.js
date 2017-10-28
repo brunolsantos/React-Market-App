dda24c8387f95425744c16148d973c9fc5e7dc04
Bruno Lucas <bruno.lucas14@hotmail.com>
2017-10-28 04:51:23 -0200

    Added shop items card

--- html_page/script.js
+++ html_page/script.js
@@ -5,13 +5,28 @@ $(function() {
 			var colorAfter = 'rgba(0, 0, 0, 0)';
 			$(this)
             .css("backgroundColor", colorBefore)
-            .animate({ backgroundColor: colorAfter }, 200);
+			.animate({ backgroundColor: colorAfter }, 200);
+			hidePopup();
 		}else{
 			var colorBefore = $(".fa-shopping-cart").css('background-color');
 			var colorAfter = 'rgba(68, 74, 136, 1)';
 			$(this)
             .css("backgroundColor", colorBefore)
-            .animate({ backgroundColor: colorAfter }, 200);		
+			.animate({ backgroundColor: colorAfter }, 200);
+			showPopup();	
 		}
 	})
+
+	function hidePopup() {
+		$(".cart-products").hide();
+	}
+		
+	function showPopup() {
+		$(".cart-products").show();
+	}
+	function callback() {
+		setTimeout(function() {
+		  $( "#effect" ).removeAttr( "style" ).hide().fadeIn();
+		}, 1000 );
+	  };
 });
